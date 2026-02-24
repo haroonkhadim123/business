9f2a42da0a0e70b02ae947cd4448703f8be91333
Haroon Khadim <haroonkhadim971@gmail.com>
2026-02-24 05:55:15 +0500

    Change 'Brands' to 'About Us' in aboutbanner.js

--- app/component/aboutbanner.js
+++ app/component/aboutbanner.js
@@ -73,7 +73,7 @@ export default function Aboutbanner() {
             Home
           </Link>
           <span>/</span>
-          <span className="text-white font-semibold">Brands</span>
+          <span className="text-white font-semibold">About Us</span>
         </motion.div>
 
       </div>
