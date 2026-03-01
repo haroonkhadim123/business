5b65949029a246737b88147db11923f2acf5c780
Haroon Khadim <haroonkhadim971@gmail.com>
2026-03-01 15:35:17 +0500

    Fix overflow-y style in brand management grid

--- app/admin/manage-brands/page.js
+++ app/admin/manage-brands/page.js
@@ -95,7 +95,7 @@ export default function ManageBrands() {
     <div className="p-2  lg:p-8 ">
       <h1 className="text-3xl font-bold mb-8">Manage Brands</h1>
 
-      <div className="grid md:grid-cols-2 gap-6 h:[70vh] md:h-[60vh] hide-scrollbar p-2  overflow-y-auto  ">
+      <div className="grid md:grid-cols-2 gap-6 h:[70vh] md:h-[60vh] hide-scrollbar  overflow-y-auto  ">
         {brands.map((brand, index) => (
           <motion.div
             key={brand.id}
