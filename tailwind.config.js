5437de074646e1290fd1e0a139c3438f1aa0a99c
haroonkhadim123 <201428119+haroonkhadim123@users.noreply.github.com>
2026-03-26 11:43:09 +0500

    changes in theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,9 @@
-// tailwind.config.js
 module.exports = {
+  darkMode: 'class', // important!
   content: [
     "./app/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {},
-  },
+  theme: { extend: {} },
   plugins: [],
 };
