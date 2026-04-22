1ba2cba79c574b2c02311d0e463b956d3c968d81
haroonkhadim123 <201428119+haroonkhadim123@users.noreply.github.com>
2026-04-22 09:50:31 +0500

    chnages in under-construction page

--- app/under-construction/page.js
+++ app/under-construction/page.js
@@ -1,13 +1,22 @@
+// app/page.js
 export const metadata = {
   title: "Under Construction",
   robots: {
-    index: false, // ❗ Google ko block
+    index: false,
   },
 };
 
 export default function Page() {
   return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
+    <div
+      style={{
+        textAlign: "center",
+        marginTop: "100px",
+        padding: "20px",
+        minHeight: "100vh", // Full viewport height
+        backgroundColor: "#fff", // Solid background
+      }}
+    >
       <h1>🚧 Website Under Construction</h1>
       <p>We are working on it. Coming soon!</p>
     </div>
