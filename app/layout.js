1a78341506625a8df002718071dee85e5f2cbb6f
haroonkhadim123 <201428119+haroonkhadim123@users.noreply.github.com>
2026-05-16 17:49:47 +0500

    new changes

--- app/layout.js
+++ app/layout.js
@@ -139,35 +139,34 @@ export default function RootLayout({ children }) {
       data-scroll-behavior="smooth"
       className="scroll-smooth"
     >
-      <head>
-          {/*<!-- Google tag (gtag.js) -->*/}
-<Script
-  src="https://www.googletagmanager.com/gtag/js?id=G-S08L9CKXV2"
-  strategy="afterInteractive"
-/>
+    
+        
+<head>
+  {/* Google tag (gtag.js) */}
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-S08L9CKXV2"
+    strategy="afterInteractive"
+  />
 
-<Script id="google-analytics" strategy="afterInteractive">
-  {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', 'G-S08L9CKXV2');
-  `}
-</Script>
-          
-          
-        {/* ✅ Google Tag Manager */}
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-PW42MT3J');
-            `,
-          }}
-        />
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-S08L9CKXV2');
+    `}
+  </Script>
+
+  {/* Google Tag Manager */}
+  <Script id="google-tag-manager" strategy="afterInteractive">
+    {`
+      (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+      })(window,document,'script','dataLayer','GTM-PW42MT3J');
+    `}
+  </Script>
         
         {/* ✅ Preconnect for performance */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
